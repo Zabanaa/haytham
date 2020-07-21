fc97f88eb0c147075b69a340aa18ab27ac7d3b24
Zabana <karim.cheurfi@gmail.com>
2020-07-21 13:22:29 +0200

    setup basic commands

--- flask/app/__init__.py
+++ flask/app/__init__.py
@@ -1,27 +1,34 @@
 import os
 from flask import Flask
-from app.example_blueprint.routes import example_blueprint
+from flask_bcrypt import Bcrypt
+from flask_cors import CORS
 
+crypt = Bcrypt()
 
 def create_app(config_obj='Config'):
 
     app = Flask(__name__, instance_relative_config=True)
     app.config.from_object(f'app.config.{config_obj}')
 
-    from app.database import db, migrate
-    db.init_app(app)
+    from app.database import db, migrate, ma
+
+    # import blueprints
+    from app.example_blueprint.routes import example_blueprint
+
+    # this is where you would import cli commands if any
+
 
     with app.app_context():
-        try:
-            db.create_all()
-        except AttributeError as e:
-            sys.exit("DB ERROR {}".format(e))
+        CORS(app, resources={r"/*": {"origins": "*"}})
 
-    migrate.init_app(app, db)
-    app.register_blueprint(example_blueprint)
+        db.init_app(app)
+        ma.init_app(app)
+        crypt.init_app(app)
+        migrate.init_app(app, db)
 
-    @app.route('/')
-    def index():
-        return "Hello, thanks for using haytham"
+        # register all blueprints
+        app.register_blueprint(example_blueprint)
+
+        # setup flask mail below
 
     return app
